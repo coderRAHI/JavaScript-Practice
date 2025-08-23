ed52aff40754a214f979570af00fa350692eb44c
Himanshu Gangwar <himanshugangwar335@gmail.com>
2025-08-23 13:30:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+//  Topic 1st VARIABLES
+var number = 18;
+console.log( "var variable number is :", number);
+console.log("type of var variable is :", typeof number);
+
+let Number = 19;
+console.log("let variable number is : " ,Number);
+console.log("type of let variable is :" , typeof Number);
+
+const title = 'himanshu';
+console.log("const title namer is :",title);
+console.log("type of const variable is :" , typeof title);
+
+// topic 2nd DATATYPES
+
+// 1 Number Datatype
+
+let NUMBER = 12;
+console.log("Number is :",NUMBER);
+console.log("type variable is :" , typeof NUMBER);
+
+// 2 string Datatype
+
+let string='Himanshu Gangwar';
+console.log("String is: ",string);
+console.log("type variable is :" , typeof string);
+
+// 3 boolean Datatype
+
+let isAdult=true;
+let isCool=false;
+
+console.log("Boolean is :",isAdult);
+console.log("type variable is :" , typeof isAdult);
+
+
+console.log("Boolean is :",isCool);
+console.log("type variable is :" , typeof isCool);
+
+// 4. Null Datatype
+
+let variable = null;
+console.log("number is :", null);
+console.log("type variable is :" , typeof variable);
+
+//  5. Undefined Datatype
+
+let VARIABLE = undefined;
+console.log("number is :", undefined);
+console.log("type variable is :" , typeof VARIABLE);
+
+// Topic 3rd Operators
+
+// Arithematic Operators.
+let num1 = 5;
+num2 = 2;
+
+console.log("The Given Num1 And Num2 resp is:" , num1 , num2);
+console.log("Addition of two number is : " ,num1 + num2);
+console.log("subtraction of two number is : " ,num1 - num2);
+console.log("Multiplication of two number is : " ,num1 * num2);
+console.log("Division of two number is : " ,num1 / num2);
+console.log("Modulus of two number is : " ,num1 % num2);
+console.log("Exponentiation of two number is : " ,num1 ** num2);
+
+// Comparision Operators 
+
+let a = 2;
+b = 5;
+console.log("The Given values of 'a' And 'b' resp is:" , a , b);
+console.log("a is equal to b :", a===b);
+console.log("a is equal to b :", a!==b);
+console.log("a is greater than b :", a>b);
+console.log("a is less than b :", a<b);
+console.log("a is greater than equal to b :", a>=b);
+console.log("a is less than equal to b :", a<=b);
+
+
+// Logical operators
+
+let x = 2;
+y = 5;
+console.log("The Given values of 'x' And 'y' resp is:" , x , y);
+
+
+
+
+
